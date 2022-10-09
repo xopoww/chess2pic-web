7b19163dd0f9be0bd5d39f5424d5ecde959a3333
Арсений Хорошилов <horoshilov.aa@phystech.edu>
2022-10-09 21:17:38 +0600

    add api proxy

--- back/index.js
+++ back/index.js
@@ -5,6 +5,17 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, "..", "front", "dist", "spa")));
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'http://localhost:65000',
+    pathRewrite: {'^/api': ''},
+    changeOrigin: true,
+  })
+);
+
 const port = 9000;
 app.listen(port, () => {
     console.log(`HTTP server listening on :${port}`);
